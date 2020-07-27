c96854c3319a6b378c153e7475d1d0c504e0508e
Natasa66 <nspelicova@nextra.sk>
2020-07-27 13:50:50 +0200

    hviezdickova pyramida (aj obratena) cez for a while

---
+++ importy/mini_kalkulacka.py
@@ -0,0 +1,22 @@
+def spocitaj(cislo1, cislo2) :
+    """ Vrati spocitane cisla
+    :param cislo1:  int
+    :param cislo2:  int
+    :return:  int
+    """
+    # if isinstance(cislo1, str) and isinstance(cislo2, str):
+    #     return False
+    # if isinstance(cislo1, int) and isinstance(cislo2, str):
+    #     return False
+    # if isinstance(cislo1, str) and isinstance(cislo2, int):
+    #     return False
+    if isinstance(cislo1,int) and isinstance(cislo2,int):
+        return int(cislo1 + cislo2)
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    print(spocitaj(2, 6))
+    print(spocitaj(5, 2))
+    print(spocitaj("kj",6))
