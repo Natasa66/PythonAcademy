ff207c370163e5eae7486d334cca49430efa0491
Natasa66 <nspelicova@nextra.sk>
2020-08-02 16:24:58 +0200

    generator_nahodneho_textu fumkcia ktora z neho vytiahne cisla delitelne 2-mi

--- generator_nahodneho_textu.py
+++ generator_nahodneho_textu.py
@@ -15,7 +15,9 @@ def generator_nahodneho_textu(len: int):
     return random_text
 
 # viem to aj uplne sofistikovane v jednom prikaze
-
+# return "".join(
+    #     random.choices(string.ascii_lowercase+string.ascii_uppercase+string.digits+"?!* .", k=dlzka)
+    # ) if isinstance(dlzka, int) else None
 
 # uloha 2 :vytvor funkciu, ktora z lubovolneho textu vytiahne do zoznamu (list)
 # #vsetky cisla delitelne dvomi
@@ -25,13 +27,14 @@ def vytiahni_cisla_delitelne_dvomi(text_n:str):
     :param text_n: str
     :return: list / False
     """
-    if not isinstance(text,str):
+    if not isinstance(text_n,str):
         return False
 
    # dlzka_textu = len(text)
+    # navrat_zoznam = [int(x) for x in text if x.isnumeric() and int(x) % 2 == 0]  # list comprehension
     navrat_zoznam = []
     for znak in text_n:
-        if znak.isnumeric() and int(znak) % 2 == 0:
+        if znak.isnumeric() and int(znak) % 2 == 0 and int(znak) != 0:
             navrat_zoznam.append(int(znak))
 
     return navrat_zoznam
@@ -41,15 +44,17 @@ def vytiahni_cisla_delitelne_dvomi(text_n:str):
 
 
 if __name__ == "__main__":
-    dlzka = input("zadaj dlzku retazca ")
-    if dlzka.isnumeric():
-        dlzka = int(dlzka)
-    else:
-        dlzka = None
+    # uloha 1
+    dlzka = int(input("zadaj dlzku retazca "))
+#    if dlzka.isnumeric():
     text = generator_nahodneho_textu((dlzka))
+    print(f" ULOHA1: {text}")
+    #uloha 2
+    # Vytvorte funkciu, ktora z lubovolneho textu, vytiahne do zoznamu (list)
+    # vsetky cisla delitelne 2
     delitelne_dvomi = vytiahni_cisla_delitelne_dvomi(text)
-    print(text)
-    print(delitelne_dvomi)
+    print("-"*50)
+    print(f"Zoznam cisel delitelnych 2: {delitelne_dvomi}")
 
 # tento if sa da napisat aj kratsie
 # dlzka = int(dlazka) if dlzka.isnumeric() else None
