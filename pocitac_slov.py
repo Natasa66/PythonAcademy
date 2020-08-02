c25a51eea9a56c7f1f2d1b8bf86f144b1823189c
Natasa66 <nspelicova@nextra.sk>
2020-08-02 09:05:56 +0200

    tri sposoby ako spocitat slova v texte

--- pocitac_slov.py
+++ pocitac_slov.py
@@ -1,15 +1,39 @@
 import urllib.request
 import ssl
-gcontext = ssl.SSLContext()
-# data = str(
-#     urllib.request.urlopen(
-#         "https://raw.githubusercontent.com/tomaspekarovic/PythonAcademy/master/Lekcia5/random_file.txt",
-#         context=gcontext).read().decode("utf-8")
-# )
+from collections import Counter
 
-# data = str(
-#     urllib.request.urlopen(
-#         "https://raw.githubusercontent.com/tomaspekarovic/PythonAcademy/master/Lekcia5/random_text.txt",
-#         context=gcontext).read().decode("utf-8")
-# )
-# print(data)
+gcontext = ssl.SSLContext()
+data = str(
+    urllib.request.urlopen(
+        "https://raw.githubusercontent.com/tomaspekarovic/PythonAcademy/master/Lekcia5/random_file.txt",
+        context=gcontext).read().decode("utf-8")
+)
+
+if __name__ == __main__:
+    znaky = {}
+    # 1 sposob
+    for znak in data:
+        if znak not in znaky.keys():
+            znaky[znak] = 1
+        else:
+            znaky[znak] = znaky[znak] + 1
+    print("1 sposob: ")
+    print(znaky)
+
+    # 2 sposob
+    del znaky
+    znaky = {}
+    for znak in data:
+        if znaky.get(znak, None):
+            znaky[znak] += 1
+        else:
+            znaky[znak] = 1
+    print("2 sposob: ")
+    print(znaky)
+
+
+    # 3 sposob
+    del znaky
+    znaky = Counter(data)
+    print("3 sposob: ")
+    print(dict(znaky))
