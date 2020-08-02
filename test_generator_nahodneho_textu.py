82690cb8b0f583e7634d87248907618c129549cc
Natasa66 <nspelicova@nextra.sk>
2020-08-02 16:28:10 +0200

    test pre generator_nahodneho_textu a funkciu, ktora z neho vytiahne cisla delitelne 2-mi

--- test_generator_nahodneho_textu.py
+++ test_generator_nahodneho_textu.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from generator_nahodneho_textu import generator_nahodneho_textu
+from generator_nahodneho_textu import generator_nahodneho_textu, vytiahni_cisla_delitelne_dvomi
 
 
 class TestGeneratorNahodnehoTextu(TestCase):
@@ -18,3 +18,26 @@ class TestGeneratorNahodnehoTextu(TestCase):
     def test_generator_nahodneho_textu_True(self):
         result = generator_nahodneho_textu(5)
         self.assertIsInstance(result, str)
+        self.assertEqual(len(result),5)
+
+
+class TestVytiahniCislaDelitelneDvomi(TestCase):
+    def test_vytiahni_cisla_delitelne_dvomi_int(self):
+        result=vytiahni_cisla_delitelne_dvomi(5)
+        self.assertFalse(result)
+
+    def test_vytiahni_cisla_delitelne_dvomi_list(self):
+        result=vytiahni_cisla_delitelne_dvomi([1,6,"greta6"])
+        self.assertFalse(result)
+
+    def test_vytiahni_cisla_delitelne_dvomi_tuple(self):
+        result=vytiahni_cisla_delitelne_dvomi((1,6,"greta6"))
+        self.assertFalse(result)
+
+    def test_vytiahni_cisla_delitelne_dvomi_string(self):
+        result=vytiahni_cisla_delitelne_dvomi("greta6")
+        self.assertTrue(result)
+
+    def test_vytiahni_cisla_delitelne_dvomi_correct_result(self):
+        result=vytiahni_cisla_delitelne_dvomi("g123reta6K45Lnh76")
+        self.assertEqual(result, [2,6,4,6,])
